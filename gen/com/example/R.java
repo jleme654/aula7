ea063084cc060f1d598b0cf701c6686baee5710c
jleme654 <julioromao3@gmail.com>
2016-02-17 13:36:45 -0200

    Version 1.2 - Menus e SubMenus

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -1360,36 +1360,37 @@ public final class R {
         public static final int zoom_plate=0x7f02052d;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
         public static final int container=0x7f090000;
+        public static final int info_details=0x7f090005;
         public static final int mStatus=0x7f090002;
         public static final int mTitulo=0x7f090001;
-        public static final int menu_add=0x7f090004;
-        public static final int menu_close_clear_cancel=0x7f090007;
-        public static final int menu_friendslist=0x7f090005;
-        public static final int menu_search=0x7f090006;
+        public static final int menu_add=0x7f090003;
+        public static final int menu_agenda=0x7f090004;
+        public static final int menu_close_clear_cancel=0x7f090008;
+        public static final int menu_friendslist=0x7f090006;
+        public static final int menu_search=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int menu_principal=0x7f080001;
+        public static final int menu_principal=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int delete=0x7f060008;
-        public static final int menu_add=0x7f060003;
-        public static final int menu_agenda=0x7f060004;
-        public static final int menu_block=0x7f060005;
-        public static final int menu_blocked_user=0x7f060006;
-        public static final int menu_close_clear_cancel=0x7f060007;
-        public static final int menu_friendslist=0x7f060009;
-        public static final int menu_home=0x7f06000a;
-        public static final int menu_info_details=0x7f06000b;
-        public static final int menu_search=0x7f06000c;
+        public static final int delete=0x7f060009;
+        public static final int menu_add=0x7f060004;
+        public static final int menu_agenda=0x7f060005;
+        public static final int menu_block=0x7f060006;
+        public static final int menu_blocked_user=0x7f060007;
+        public static final int menu_close_clear_cancel=0x7f060008;
+        public static final int menu_friendslist=0x7f06000a;
+        public static final int menu_home=0x7f06000b;
+        public static final int menu_info_details=0x7f06000c;
+        public static final int menu_search=0x7f06000d;
+        public static final int status=0x7f060003;
         public static final int titulo=0x7f060002;
     }
     public static final class style {
--- src/com/example/MainActivity.java
+++ src/com/example/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -16,35 +22,70 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_principal, menu);
 		return true;
 	}
 
+	public void getMenuSelecionado(String msg) {
+		TextView status = (TextView) findViewById(R.id.mStatus);
+		status.setText(msg);
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch (item.getItemId()) {
+//		case R.id.menu_add:
+//			getMenuSelecionado("Adicionar Contato Selecionado");
+//			return true;
+		case R.id.menu_friendslist:
+			getMenuSelecionado("Lista de Contatos");
 			return true;
+		case R.id.menu_search:
+			AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+			dialogo.setMessage("Menu Busca Contato selecionado");
+			dialogo.setTitle("Buscar Contato");
+			dialogo.setIcon(R.drawable.ic_menu_search);
+			dialogo.setNeutralButton("OK", null);
+			dialogo.show();
+			return true;
+		case R.id.menu_close_clear_cancel:
+//			getMenuSelecionado("Opção fechar selecionado");
+			fecharAtividade();
+			return true;
+		default:
+//			getMenuSelecionado("Outro menu selecionado");
+			return false;
 		}
-		return super.onOptionsItemSelected(item);
+	}
+
+	public void fecharAtividade() {
+		AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+		dialogo.setMessage("Deseja encerrar?");
+		dialogo.setPositiveButton("Sim", new OnClickListener() {		
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+		});
+	    dialogo.setNegativeButton("Nao", new OnClickListener() {		
+			public void onClick(DialogInterface dialog, int which) {
+				TextView status = (TextView) findViewById(R.id.mStatus);
+				status.setText("Botao nao selecionado");
+			}
+		});	
+	    dialogo.setTitle("Fechar atividade");
+	    dialogo.setIcon(R.drawable.ic_menu_info_details);
+	    dialogo.show();
 	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
